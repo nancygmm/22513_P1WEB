674323b422701de0e982b4031e6a37a6b249bdd2
Nancy Mazariegos <nancymazariegos03@gmail.com>
2024-04-30 18:47:51 -0600

    Footer component
    Se creó el componente footer.

---
+++ src/Componentes/Footer.jsx
@@ -0,0 +1,13 @@
+function Footer() {
+    return (
+      <footer className="footer">
+        <div className="contenedor">
+          <div className="barra">
+            <p>Todos los derechos reservados 2024</p>
+          </div>
+        </div>
+      </footer>
+    );
+  }
+
+export default Footer
